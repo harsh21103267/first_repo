26811eff186ad63ca7f22d02ca1dadfd1c875859
harsh21103267 <141023490+harsh21103267@users.noreply.github.com>
2023-07-31 15:43:56 +0530

    Update selection.py

--- selection.py
+++ selection.py
@@ -1,53 +1,23 @@
-// C++ program for implementation of
-// selection sort
-#include <bits/stdc++.h>
-using namespace std;
+# Python program for implementation of Selection
+# Sort
+import sys
+A = [64, 25, 12, 22, 11]
 
-// Function for Selection sort
-void selectionSort(int arr[], int n)
-{
-	int i, j, min_idx;
+# Traverse through all array elements
+for i in range(len(A)):
+	
+	# Find the minimum element in remaining
+	# unsorted array
+	min_idx = i
+	for j in range(i+1, len(A)):
+		if A[min_idx] > A[j]:
+			min_idx = j
+			
+	# Swap the found minimum element with
+	# the first element	
+	A[i], A[min_idx] = A[min_idx], A[i]
 
-	// One by one move boundary of
-	// unsorted subarray
-	for (i = 0; i < n - 1; i++) {
-
-		// Find the minimum element in
-		// unsorted array
-		min_idx = i;
-		for (j = i + 1; j < n; j++) {
-			if (arr[j] < arr[min_idx])
-				min_idx = j;
-		}
-
-		// Swap the found minimum element
-		// with the first element
-		if (min_idx != i)
-			swap(arr[min_idx], arr[i]);
-	}
-}
-
-// Function to print an array
-void printArray(int arr[], int size)
-{
-	int i;
-	for (i = 0; i < size; i++) {
-		cout << arr[i] << " ";
-		cout << endl;
-	}
-}
-
-// Driver program
-int main()
-{
-	int arr[] = { 64, 25, 12, 22, 11 };
-	int n = sizeof(arr) / sizeof(arr[0]);
-
-	// Function Call
-	selectionSort(arr, n);
-	cout << "Sorted array: \n";
-	printArray(arr, n);
-	return 0;
-}
-
-// This is code is contributed by rathbhupendra
+# Driver code to test above
+print ("Sorted array")
+for i in range(len(A)):
+	print("%d" %A[i],end=" , ")
